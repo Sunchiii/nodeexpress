b9e3f9a27c00f1bccb9e03072878e756756759fa
sunchi <rachanlamoolphal@gmail.com>
2021-10-26 16:44:55 +0700

    create data task for test

--- index.js
+++ index.js
@@ -2,9 +2,29 @@ const express = require('express')
 const path = require('path')
 const app = express()
 
-app.get('/',(req,res)=>{
-    res.sendFile(path.join(__dirname,'public','index.html'))
+//api
+const task = [
+    {
+        id:1,
+        name:'homework',
+        disscrip:'make homework'
+    },
+    {
+        id:2,
+        name:'work',
+        disscrip:'make work for money'
+    }
+]
+app.get('/api/task',(req,res)=>{
+
 })
+
+// app.get('/',(req,res)=>{
+//     res.sendFile(path.join(__dirname,'public','index.html'))
+// })
+
+//set static path
+app.use(express.static(path.join(__dirname,'public')))
 const port = process.env.PORT || 5000;
 
 app.listen(port ,()=> console.log(`server has runing on port ${port}`))
