94e945f9f87aba41358f35b6013c78aba1b0e2fd
sunchi <rachanlamoolphal@gmail.com>
2021-10-26 17:42:34 +0700

    filter data from task with params

--- index.js
+++ index.js
@@ -5,13 +5,19 @@ const app = express()
 
 
 //create function for use middleware 
-const store = (req,res,next)=>{
-    console.log("this middleware")
-    next()
-}
-app.use(store)
+// const store = (req,res,next)=>{
+//     console.log("this middleware")
+//     next()
+// }
+// app.use(store)
+
+
 //api
 app.get('/api/task',(req,res)=>res.json(task))
+
+app.get('/api/task/:id',(req,res)=>{
+    res.json(task.filter(task => task.id === parseInt(req.params.id)))
+})
 
 // app.get('/',(req,res)=>{
 //     res.sendFile(path.join(__dirname,'public','index.html'))
