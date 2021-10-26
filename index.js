c48298a77ac978ac01eacdf6f4e768e479b8943f
sunchi <rachanlamoolphal@gmail.com>
2021-10-26 16:32:10 +0700

    create index.html and sendfile

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express')
-
+const path = require('path')
 const app = express()
+
+app.get('/',(req,res)=>{
+    res.sendFile(path.join(__dirname,'public','index.html'))
+})
 const port = process.env.PORT || 5000;
 
 app.listen(port ,()=> console.log(`server has runing on port ${port}`))
